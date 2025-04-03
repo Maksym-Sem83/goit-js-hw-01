bd32db7fd8a845e0cb3b7a1c4b92de9543bc4182
Maksym Semenov <max_semenov@ukr.net>
2025-04-03 13:20:25 +0300

    add task-2, task-3

--- js/task-1.js
+++ js/task-1.js
@@ -1,5 +1,5 @@
 function makeTransaction(quantity, pricePerDroid) {
-    return `You ordered ${quantity} droids worth ${quantity * pricePerDroid} credits!`
+    return `You ordered ${quantity} droids worth ${quantity * pricePerDroid} credits!`;
 }
 
 makeTransaction(5, 3000);
